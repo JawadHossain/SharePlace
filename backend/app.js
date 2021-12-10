f46ae03afe8f708687bdc8994dc3de4b49911ebe
Jawad Hossain <jhossain@ualberta.ca>
2021-12-09 21:39:07 -0700

    Set resposne json on backend error

--- backend/app.js
+++ backend/app.js
@@ -46,9 +46,8 @@ app.use((error, req, res, next) => {
         return next(error)
     }
 
-    res.status(error.code || 500).json({
-        message: error.message || 'An unknown error occurred!'
-    })
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
 })
 
 mongoose
